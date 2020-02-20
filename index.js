6d13ac0c88a73f0f5dd326c67c505f54fa8f77f5
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-02-20 23:35:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-// add solution here
-// 1. Create a function `theBeatlesPlay`, which accepts two parameters--an array of
-//   musicians and an array of instruments. 
-
-//     * Create a variable with an empty array.
-//     * Use a `for loop`, which iterates over the array of musicians. Be careful
-//       about what value you set your `counter` variable to store. (Hint: Think
-//       about what the first index of an array is). 
-//     * The first time through the loop, it should create a string using the _first_
-//       index of the musicians array **and** the first index of the instruments
-//       array: `"John Lennon plays guitar"`. 
-//     * Add this string to the empty array you created. The loop should make the
-//       same sentence for every member of the musicians array. 
-//     * The function should return the array of new strings containing what
-//       instruments each musician plays.
 
 function theBeatlesPlay(arrMusic,arrInstr) {
   
@@ -36,7 +21,6 @@ function johnLennonFacts(arr) {
   return facts;
 }
   
-// Create a function `iLoveTheBeatles` which accepts a number as a parameter. 
 function iLoveTheBeatles(num) {
     let phrase = [];
     let str = 'I love the Beatles!';
