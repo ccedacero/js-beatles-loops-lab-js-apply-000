ca3c8d7306c5533d0702416f40e50a68dd9c6851
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-02-20 23:15:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,4 +24,36 @@ function theBeatlesPlay(arrMusic,arrInstr) {
   } return theyPlay;
 }
 
+// Create a function `johnLennonFacts`. 
+
+function johnLennonFacts(arr) {
+  let facts = [];
+  let i = 0;
+  while(i <=arr.length) {
+     let str = arr[i] + '!!!';
+     facts.push(str);
+     i++;
+  }
+  return facts;
+}
+  
+  
+//     * This function will accept one argument, an array of facts about John
+//       Lennon (note that it might not be exactly the following facts):
+
+//     ```js
+//     const facts = [
+//       "He was the last Beatle to learn to drive",
+//       "He was never a vegetarian",
+//       "He was a choir boy and boy scout",
+//       "He hated the sound of his own voice"
+//     ];
+//     ```
+
+//     * Use a while loop to loop over the facts array and add `"!!!"` to the end of
+//     every fact.
+
+//     * Return an array of strings with exclamation points.
+
+
 
